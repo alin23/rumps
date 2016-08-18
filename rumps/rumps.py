dbf3c6f5a175dfa42986b2cd50dfaf9acfde62ed
alin23 <alin.p32@gmail.com>
2016-08-18 12:21:13 +0300

    PEP8 format

--- rumps/rumps.py
+++ rumps/rumps.py
@@ -12,19 +12,34 @@ try:
 except ImportError:
     _NOTIFICATIONS = False
 
-from Foundation import (NSDate, NSTimer, NSRunLoop, NSDefaultRunLoopMode, NSSearchPathForDirectoriesInDomains,
-                        NSMakeRect, NSLog, NSObject)
-from AppKit import NSApplication, NSStatusBar, NSMenu, NSMenuItem, NSAlert, NSTextField, NSImage
-from PyObjCTools import AppHelper
-
-import inspect
 import os
 import sys
 import weakref
-
 from collections import Mapping, Iterable
+
+from AppKit import (
+    NSMenu,
+    NSAlert,
+    NSImage,
+    NSMenuItem,
+    NSStatusBar,
+    NSTextField,
+    NSApplication
+)
+from Foundation import (
+    NSLog,
+    NSDate,
+    NSTimer,
+    NSObject,
+    NSRunLoop,
+    NSMakeRect,
+    NSDefaultRunLoopMode,
+    NSSearchPathForDirectoriesInDomains
+)
+from PyObjCTools import AppHelper
+
 from .utils import ListDict
-from .compat import text_type, string_types, iteritems
+from .compat import iteritems, text_type, string_types
 
 _TIMERS = weakref.WeakKeyDictionary()
 separator = object()
@@ -404,7 +419,7 @@ class Menu(ListDict):
                                          'submenu'.format(n, depth, len(tuple(ele))))
                     menuitem = MenuItem(menuitem)
                     menu.add(menuitem)
-                    parse_menu(submenu, menuitem, depth+1)
+                    parse_menu(submenu, menuitem, depth + 1)
 
                 # menu item / could be visual separator where ele is None or separator
                 else:
@@ -648,6 +663,7 @@ class MenuItem(Menu):
 
 class SeparatorMenuItem(object):
     """Visual separator between :class:`rumps.MenuItem` objects in the application menu."""
+
     def __init__(self):
         self._menuitem = NSMenuItem.separatorItem()
 
@@ -664,6 +680,7 @@ class Timer(object):
                      :class:`rumps.Timer` object as its only parameter.
     :param interval: The time in seconds to wait before calling the `callback` function.
     """
+
     def __init__(self, callback, interval):
         self.set_callback(callback)
         self._interval = interval
